800a5d66fe70ed69b5403ee3546acf287ba88125
kien1nguyen <>
2021-04-23 23:14:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+states_of_america = ["Delaware","Pennsylvannia","New York","Connecticut","Massachusetts","Mississippi","Utal"]
+
+print(states_of_america)  # print all
+print (states_of_america[-1])   # print the last item
+
+states_of_america.append("Taxes")
+print(states_of_america)    # add to the last position : Taxes
+
+states_of_america.extend(["Hawai","Alaska","New Mexico"])
+print (states_of_america)   # add 3 states to the end of the list
